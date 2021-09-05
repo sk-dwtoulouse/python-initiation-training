fe6bb598b8d29ca30cd0ed4f869148e87b694342
Steve Kossouho <steve.kossouho@gmail.com>
2021-09-06 01:43:10 +0200

    Change path for GUI demo
    Changed path for GUI demo.

--- demos/99-extra/commandline-arguments/source/argparse_demo.py
+++ demos/99-extra/commandline-arguments/source/argparse_demo.py
@@ -1,7 +1,8 @@
 import argparse
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Parser de demo")
+    # Argparse demo: accept running this script with options like command tools.
+    parser = argparse.ArgumentParser(description="Demonstration argument parser")
     parser.add_argument("--plop", required=True, help="Argument inutile", type=int)
     parser.add_argument("--plip", required=False, default=None, help="Autre argument inutile", type=int)
     options = parser.parse_args()
